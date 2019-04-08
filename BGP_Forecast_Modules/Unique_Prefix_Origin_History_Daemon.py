f36e9451b803f7a532baa8b4e6d85c699aa410fc
Xinyu <xinyuwang1209@gmail.com>
2019-04-08 03:27:59 +0000

    Add unique_prefix_origin_generator

---
+++ BGP_Forecast_Modules/Unique_Prefix_Origin_History_Daemon.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+import daemon
+
+class Unique_Prefix_Origin_History_Daemon()
+    def __init__(self):
+        pass
+    def check_exist_daemon(self):
+        pass
+
+    def run_daemon(self):
+        pass
+
+    def update_prefix_origin_history_periodically(self):
+        pause.until(datetime(2015, 8, 12, 2))
